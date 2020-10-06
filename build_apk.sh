d0dfb69fa4602f99eeaaec1f2fd12b8ecf037d41
Pushpender Rana <pushpender661@gmail.com>
2020-10-07 04:20:53 +0530

    removed branch variable

--- build_apk.sh
+++ build_apk.sh
@@ -4,9 +4,7 @@ PATH=$PATH:/home/build/tools/bin:/home/build/tools/platform-tools:/home/build/fl
 HEAD_COMMIT_ID=`git rev-parse HEAD`
 echo "Head is at "$HEAD_COMMIT_ID
 TAG_NAME=`git tag --points-at $HEAD_COMMIT_ID`
-BRANCH=`git branch --show-current`
 echo "TAG_NAME is "$TAG_NAME
-echo "BRANCH_NAME is "$BRANCH
 echo "At Line 11"
 API_JSON=$(printf '{"tag_name": "%s","target_commitish": "master","name": "TOTP-Flutter-%s","body": "Release of version %s","draft": false,"prerelease": true}' $TAG_NAME $TAG_NAME $TAG_NAME)
 echo $API_JSON
